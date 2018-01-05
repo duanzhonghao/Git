2e847f09808e785742f403cfd561b77535ba836b
duanzhonghao <851679262@qq.com>
2018-01-05 16:45:52 +0800

    新建了项目

---
+++ src/com/zhiyou/git/GitDemo.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.zhiyou.git;
+
+/**  
+* @ClassName: GitDemo  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:41:25
+*    
+*/
+public class GitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("git测试");
+	}
+}
