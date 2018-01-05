674786aae6c64cf0728be1864185eaf8c1f4caa4
duanzhonghao <851679262@qq.com>
2018-01-05 16:48:07 +0800

    第一次修改文件

---
+++ src/com/zhiyou/git/GitDemo02.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.zhiyou.git;
+
+/**  
+* @ClassName: GitDemo02  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:46:45
+*    
+*/
+public class GitDemo02 {
+
+	public static void main(String[] args) {
+		System.out.println("第二次测试");
+	}
+}
